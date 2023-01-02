8e0d2cad2e47c89b3dc261a5ef538f9ed299f1d9
Eugene Masaka <eugenemasaka777@gmail.com>
2023-01-02 22:30:28 +0300

    Smart Contract

---
+++ Greeter.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+contract Greeter {
+    string public name;
+    function setName(string memory _name) public{
+        name = _name;
+    }
+    function getName() public view returns (string memory) {
+        return name;
+    }
+}
